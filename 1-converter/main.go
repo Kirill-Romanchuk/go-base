7016da34cae75112d41db5e1daaa654bcd644b6a
Kirill-Romanchuk <kirillromancuk41@mail.ru>
2025-07-04 15:37:18 +0300

    fix main.go for 4 course module

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -1,20 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func getUserInput() (USD_TO_EUR float64, USD_TO_RUB float64) {
-	fmt.Print("Введите курс доллора к евро: ")
-	fmt.Scan(USD_TO_EUR)
-	fmt.Print("Введите курс доллора к рублю: ")
-	fmt.Scan(USD_TO_RUB)
-	return USD_TO_EUR, USD_TO_RUB
+func getUserInput() (value float64, currencyFrom string, currencyTo string) {
+	fmt.Print("Введите сумму для конвертации: ")
+	fmt.Scan(&value)
+	fmt.Print("Введите исходную валюту: ")
+	fmt.Scan(&currencyFrom)
+	fmt.Print("Введите желаемую валюту: ")
+	fmt.Scan(&currencyTo)
+	return value, currencyFrom, currencyTo
 }
 
 func calculate(value float64, currencyFrom string, currencyTo string) float64 {
+	return 0.0
 }
 
 func main() {
-	var USD_TO_EUR, USD_TO_RUB = getUserInput()
-	var EUR_TO_RUB float64 = USD_TO_RUB / USD_TO_EUR
-	fmt.Println(EUR_TO_RUB)
+	var value, currencyFrom, currencyTo = getUserInput()
+	fmt.Print(calculate(value, currencyFrom, currencyTo))
 }
