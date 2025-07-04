d65d39ba600bec2225ec1ebd1e1ef0f59e99b249
Kirill-Romanchuk <kirillromancuk41@mail.ru>
2025-07-04 15:07:20 +0300

    add getUserInput in main.go

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -2,9 +2,16 @@ package main
 
 import "fmt"
 
+func getUserInput() (USD_TO_EUR float64, USD_TO_RUB float64) {
+	fmt.Print("Введите курс доллора к евро: ")
+	fmt.Scan(USD_TO_EUR)
+	fmt.Print("Введите курс доллора к рублю: ")
+	fmt.Scan(USD_TO_RUB)
+	return USD_TO_EUR, USD_TO_RUB
+}
+
 func main() {
-	const USD_TO_EUR float64 = 0.85
-	const USD_TO_RUB float64 = 79.03
+	var USD_TO_EUR, USD_TO_RUB = getUserInput()
 	var EUR_TO_RUB float64 = USD_TO_RUB / USD_TO_EUR
 	fmt.Println(EUR_TO_RUB)
 }
